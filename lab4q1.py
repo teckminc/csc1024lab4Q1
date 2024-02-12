41c052c7e45d682f8d79d6e6105ca346395c7aca
teckminc <102782040+teckminc@users.noreply.github.com>
2024-02-12 14:50:47 +0800

    First Commit
    First Commit

---
+++ lab4q1.py
@@ -0,0 +1,28 @@
+'''
+Do not remove any text from these comments
+1.	Improve the following Python program (Sample Answer for CE03 Q5) as in 
+following code:
+• Randomise the secret_num. The random number generated must be within the
+ range of 1 to 200.
+• Add an iterative function using the while loop to ask the user for a 
+decision to continue playing the game.
+'''
+
+def main():
+    secret_num = 50
+    guess_num = int(input("my guess"))
+    count = 1
+    
+    while guess_num != secret_num:
+        if guess_num < secret_num:
+            print("too low")
+        else:
+            print("too high")
+        count = count + 1
+        guess_num = int(input("have another guess"))
+
+    print(f"Well done, you took {str(count)} attempts")
+
+    return 0
+
+
---
+++ lab4q1_test.py
@@ -0,0 +1,58 @@
+import lab4q1
+from io import StringIO
+from sys import stderr
+
+def generate_value(a=0, b=0):
+  return 5
+
+def test_matching(monkeypatch, capsys):
+  monkeypatch.setattr("random.randint", generate_value)
+  number_inputs = StringIO("10\n5\nn\n")
+
+  monkeypatch.setattr('sys.stdin', number_inputs)
+  lab4q1.main()
+  captured_stdout, captured_stderr = capsys.readouterr()  
+
+  assert captured_stdout.strip().find(f'too high') != -1
+  assert captured_stdout.strip().find(f'2') != -1
+
+def test_matching2(monkeypatch, capsys):
+  monkeypatch.setattr("random.randint", generate_value)
+  number_inputs = StringIO("3\n5\nn\n")
+
+  monkeypatch.setattr('sys.stdin', number_inputs)
+  lab4q1.main()
+  captured_stdout, captured_stderr = capsys.readouterr()  
+
+  assert captured_stdout.strip().find(f'too low') != -1
+  assert captured_stdout.strip().find(f'2') != -1
+
+def test_matching3(monkeypatch, capsys):
+  monkeypatch.setattr("random.randint", generate_value)
+  number_inputs = StringIO("3\n5\ny\n6\n5\nn\n")
+
+  monkeypatch.setattr('sys.stdin', number_inputs)
+  lab4q1.main()
+  captured_stdout, captured_stderr = capsys.readouterr()  
+
+  assert captured_stdout.strip().find(f'too low') != -1
+  assert captured_stdout.strip().find(f'too high') != -1
+  assert captured_stdout.strip().find(f'2') != -1
+
+def test_matching4(monkeypatch, capsys):
+  monkeypatch.setattr("random.randint", generate_value)
+  number_inputs = StringIO("3\n5\nr\nn\n")
+
+  monkeypatch.setattr('sys.stdin', number_inputs)
+  lab4q1.main()
+  captured_stdout, captured_stderr = capsys.readouterr()  
+
+  assert captured_stdout.strip().find(f'too low') != -1
+  assert captured_stdout.strip().find(f'2') != -1
+
+def test_case2(monkeypatch, capsys):
+  with open(f"lab4q1.py") as tf:
+    head = [next(tf) for _ in range(9)]
+    s = tf.read()
+    assert(s.find("while") != -1 )
+
